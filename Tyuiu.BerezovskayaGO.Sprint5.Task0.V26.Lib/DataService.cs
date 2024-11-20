9de14f8ebde706ca2c39e1645c0f9e194bee6004
Tyuiu.BerezovskayaGO@outlook.com <Tyuiu.BerezovskayaGO@outlook.com>
2024-11-20 20:27:56 +0500

    Task0

--- Tyuiu.BerezovskayaGO.Sprint5.Task0.V26.Lib/DataService.cs
+++ Tyuiu.BerezovskayaGO.Sprint5.Task0.V26.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.BerezovskayaGO.Sprint5.Task0.V26.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{ Directory.GetCurrentDirectory()}\OutPutFileTassk0.txt";
+            string path = $@"{ Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
             double f = 0.7 * Math.Pow(x, 3) + 1.52 * Math.Pow(x, 2);
             f = Math.Round(f, 3);
             File.WriteAllText(path, Convert.ToString(f));
