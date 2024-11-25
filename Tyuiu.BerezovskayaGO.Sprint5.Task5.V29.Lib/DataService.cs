3887310d86ed7238e9d85347b0162233c8fba512
Tyuiu.BerezovskayaGO@outlook.com <Tyuiu.BerezovskayaGO@outlook.com>
2024-11-25 20:50:16 +0500

    Task5

---
+++ Tyuiu.BerezovskayaGO.Sprint5.Task5.V29.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.BerezovskayaGO.Sprint5.Task5.V29.Lib
+{
+    public class DataService : ISprint5Task5V29
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string lineReplace = line.Replace('.', ',');
+                    string[] lineArray = lineReplace.Split(' ');
+
+                    foreach (string number in lineArray)
+                    {
+                        double lineParse = double.Parse(number);
+
+                        if ((lineParse > 9)&&(lineParse <100))
+                        {
+                            res = lineParse;
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BerezovskayaGO.Sprint5.Task5.V29/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.BerezovskayaGO.Sprint5.Task5.V29.Lib;
+namespace Tyuiu.BerezovskayaGO.Sprint5.Task5.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Березовская Г. О. | АСОиУБ - 24-1";
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                                        *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла.                                                  *");
+            Console.WriteLine("* Задание #5                                                                                       *");
+            Console.WriteLine("* Вариант #29                                                                                      *");
+            Console.WriteLine("* Выполнила: Березовская Галина Олеговна | АСОиУБ - 24-1                                           *");
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                         *");
+            Console.WriteLine("* Дан файл  в котором есть набор значений. Найти минимальное целое число в файле,  которое         *");
+            Console.WriteLine("* является двузначным числом. Полученный результат вывести на консоль. У вещественных              *");
+            Console.WriteLine("* значений округлить до трёх знаков после запятой.                                                 *");
+            Console.WriteLine("*                                                                                                  *");
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                 *");
+            Console.WriteLine("****************************************************************************************************");
+
+            string path = @"C:\Users\Admin\source\repos\Tyuiu.BerezovskayaGO.Sprint5\DataSprint5\InPutDataFileTask5V29.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                       *");
+            Console.WriteLine("****************************************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Минимальное целое двузначное число = " + res);
+            Console.ReadKey();
+        }
+    }
+}
