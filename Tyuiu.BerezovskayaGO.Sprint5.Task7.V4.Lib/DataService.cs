bbfc4289661b9b8e544e9ad27dcb30fa4f0bd9de
Tyuiu.BerezovskayaGO@outlook.com <Tyuiu.BerezovskayaGO@outlook.com>
2024-11-26 20:03:22 +0500

    Task7

---
+++ Tyuiu.BerezovskayaGO.Sprint5.Task7.V4.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.BerezovskayaGO.Sprint5.Task7.V4.Lib
+{
+    public class DataService : ISprint5Task7V4
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string path2 = Path.Combine(new string[] { Path.GetTempPath(), "OutPutDataFileTask7V4.txt" });
+
+            FileInfo f = new FileInfo(path2);
+            if (f.Exists)
+            {
+                File.Delete(path2);
+            }
+
+            string result = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string inputText = reader.ReadToEnd();
+
+                // Заменяем все русские буквы на #
+                string outputText = Regex.Replace(inputText, @"[А-Яа-я]", "#");
+                return outputText;
+            }
+        }
+    }
+}
---
+++ Tyuiu.BerezovskayaGO.Sprint5.Task7.V4/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.BerezovskayaGO.Sprint5.Task7.V4.Lib;
+namespace Tyuiu.BerezovskayaGO.Sprint5.Task7.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Березовская Г. О. | АСОиУБ - 24-1";
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                                        *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов.                                                                *");
+            Console.WriteLine("* Задание #7                                                                                       *");
+            Console.WriteLine("* Вариант #4                                                                                       *");
+            Console.WriteLine("* Выполнила: Березовская Галина Олеговна | АСОиУБ - 24-1                                           *");
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                         *");
+            Console.WriteLine("* Дан файл, в котором есть набор символьных данных.                                                *");
+            Console.WriteLine("* Заменить все русские буквы на #.                                                                 *");
+            Console.WriteLine("*                                                                                                  *");
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                 *");
+            Console.WriteLine("****************************************************************************************************");
+
+            string path = @"C:\Users\Admin\source\repos\Tyuiu.BerezovskayaGO.Sprint5\DataSprint5\InPutDataFileTask7V4.txt";
+            string path2 = Path.Combine(new string[] { Path.GetTempPath(), "OutPutDataFileTask7V4.txt" });
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                       *");
+            Console.WriteLine("****************************************************************************************************");
+
+            path2 = ds.LoadDataAndSave(path);
+            Console.WriteLine(path2);
+            Console.ReadKey();
+        }
+    }
+}
